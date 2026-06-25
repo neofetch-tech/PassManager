5f653a1e19587f487ee9296f3c4f576fbbc01ed4
Liquidarium <ponchikpupsik7@gmail.com>
2026-06-25 09:01:39 +0300

    Delete code directory

--- code/cryptography.py
+++
@@ -1,5 +0,0 @@
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-cipher = Fernet(key)
-encrypted = cipher.encrypt(password.encode()).decode()
-decrypted = cipher.decrypt(encrypted.encode()).decode()
